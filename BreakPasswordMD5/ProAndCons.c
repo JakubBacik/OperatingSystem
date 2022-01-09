962727aa52fbc3387619630d893b3eb479c7301c
JakubBacik <jakub99.b@gmail.com>
2022-01-09 23:55:35 +0100

    Pass generator ver. 3

--- BreakPasswordMD5/ProAndCons.c
+++ BreakPasswordMD5/ProAndCons.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/signal.h>
+#include <ctype.h>
 #include "openssl/md5.h"
 
 # define NUMBEROFPRODUCER 4
@@ -22,9 +23,10 @@ char* slownik[10] = {"adam", "marek", "jan", "kuba", "michal", "adrian", "ola", 
 void handler (int sig);
 void producer(void *);
 void consumer(void *);
-char* Generate(char* password, int number78, int option);
+char* Generate(char* password, int number78, int option, int letter);
 char* appendCharToCharArray(char* array, char a, int option);
-char* MakePassword(char* password, int numner,  int taskid);
+char* MakePassword(char* password, int numner, int letter,  int taskid);
+char* Dictionary(char* password, int letter);
 struct Data
 {
     int numberOfBreakPass;
@@ -85,39 +87,40 @@ int main(int argc, char **argv){
 void producer(void *threadid){
     long taskid;
     taskid = (long)threadid;
-    for(int j=0; j<1 ;j++){
+    for(int j=0;;j++){
         for(int h=0; h<10; h++){
+            for(int k=0; k<3; k++){
+                char* passToMD5 = MakePassword(slownik[h], j, k, taskid);
+                printf("%s\n", passToMD5);
+        
+                unsigned char digest[32];
+                MD5(passToMD5, strlen(passToMD5), (unsigned char*)&digest); 
+                                
+                char convertFromHex[32];
+                for(int i = 0; i < 16; i++)
+                    sprintf(&convertFromHex[i*2], "%02x", (unsigned int)digest[i]);
 
-            char* passToMD5 = MakePassword(slownik[h], j, taskid);
-            printf("%s\n", passToMD5);
-    
-            unsigned char digest[32];
-            MD5(passToMD5, strlen(passToMD5), (unsigned char*)&digest); 
+                for(int i=0; i<3; i++){
+                    if(password[i][32] == '0'){
+                        if(strncmp(password[i], convertFromHex, 32)==0){
                             
-            char convertFromHex[32];
-            for(int i = 0; i < 16; i++)
-                sprintf(&convertFromHex[i*2], "%02x", (unsigned int)digest[i]);
+                            printf("Wyslanie\n");
 
-            for(int i=0; i<3; i++){
-                if(password[i][32] == '0'){
-                    if(strncmp(password[i], convertFromHex, 32)==0){
-                        
-                        printf("Wyslanie\n");
+                            pthread_mutex_lock(&put.mutex);
+                            numberofBreakPass[put.number].numberOfBreakPass=h;
+                            numberofBreakPass[put.number].BreakPass=passToMD5;
+                            put.number++;
+                            pthread_mutex_unlock(&put.mutex);
 
-                        pthread_mutex_lock(&put.mutex);
-                        numberofBreakPass[put.number].numberOfBreakPass=h;
-                        numberofBreakPass[put.number].BreakPass=passToMD5;
-                        put.number++;
-                        pthread_mutex_unlock(&put.mutex);
-
-                        pthread_mutex_lock(&nready.mutex);
-                        if (nready.nready == 0){
-                            pthread_cond_signal(&nready.cond);
+                            pthread_mutex_lock(&nready.mutex);
+                            if (nready.nready == 0){
+                                pthread_cond_signal(&nready.cond);
+                            }
+                            nready.nready++;    
+                            pthread_mutex_unlock(&nready.mutex);
                         }
-                        nready.nready++;    
-                        pthread_mutex_unlock(&nready.mutex);
+                        
                     }
-                    
                 }
             }
         }
@@ -145,27 +148,51 @@ void consumer(void *threadid){
 
 void handler (int signal_number){
     printf ("\n The number of broken passwords is %d\n \n", put.number);
+    for(int i=0; i<put.number-1; i++){
+        printf("%s \n", numberofBreakPass[put.number-1].BreakPass);
+    }
 }
 
 
-char* MakePassword(char* password, int numner, int taskid){
+char* MakePassword(char* password, int numner, int letter, int taskid){
     char* toReturn;
     switch (taskid)
     {
     case 0:
-        if(numner < 10)
-            toReturn = password;
-        else
-            toReturn = Generate(password, numner-10, 1);
+        if(numner<1){
+            if(letter == 0)
+                toReturn = Dictionary(password, 1);
+            else if(letter == 1)
+                toReturn = Dictionary(password,2);
+            else if(letter == 2)
+                toReturn = Dictionary(password,3);
+        }else{
+            if(letter == 0)
+                toReturn = Generate(password, numner-1, 1,1);
+            else if(letter == 1)
+                toReturn = Generate(password, numner-1, 1,2);
+            else if(letter == 2)
+                toReturn = Generate(password, numner-1, 1,3);
+        }
         break;
     case 1:
-        toReturn = Generate(password, numner, 2);
+        if(letter == 0)
+            toReturn = Generate(password, numner, 2,1);
+        else if(letter == 1)
+            toReturn = Generate(password, numner, 2,2);
+        else if(letter == 2)
+            toReturn = Generate(password, numner, 2,3);
         break;
     case 2:
-        toReturn = Generate(password, numner, 3);
+        if(letter == 0)
+            toReturn = Generate(password, numner, 2,1);
+        else if(letter == 1)
+            toReturn = Generate(password, numner, 2,2);
+        else if(letter == 2)
+            toReturn = Generate(password, numner, 2,3);
         break;
     case 3:
-        toReturn = Generate(password, numner, 4);
+        toReturn = Generate(password, numner, 1,4);
         break;
     
     default:
@@ -200,7 +227,7 @@ char* appendCharToCharArray(char* array, char a, int option)
 }
 
 
-char* Generate(char* password, int number78, int option) {
+char* Generate(char* password, int number78, int option, int letter) {
     char toShow[20]={' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '};
     int number1=0, number2=0;
     int number123456=0;
@@ -219,7 +246,16 @@ char* Generate(char* password, int number78, int option) {
     char *tmp;
     toReturn = (char *) calloc( (length+number123456), sizeof(char));
     for (int i = 0; i < length; i++) {
-        toReturn[i] = password[i];
+        if(letter == 1 )
+            toReturn[i] = password[i];
+
+        if(letter == 2){
+            toReturn[i] = password[i];
+            toReturn[0] = toupper(password[0]); 
+        }
+
+        if(letter == 3)
+            toReturn[i] = toupper(password[i]);
     }
 
     for (int i = 0; i < NUMBER; i++) {
@@ -257,3 +293,27 @@ char* Generate(char* password, int number78, int option) {
         return toReturn;
 
 }
+
+char* Dictionary(char* password, int letter) {
+    size_t length = strlen(password);
+    char *toReturn;
+    char *tmp;
+    toReturn = (char *) calloc( length+1, sizeof(char));
+    for (int i = 0; i < length; i++) {
+        if(letter == 1 )
+            toReturn[i] = password[i];
+
+        if(letter == 2){
+            toReturn[i] = password[i];
+            toReturn[0] = toupper(password[0]); 
+        }
+
+        if(letter == 3)
+            toReturn[i] = toupper(password[i]);
+    }
+    toReturn[length+1] = '\0';
+    return toReturn;
+
+}
+
+
